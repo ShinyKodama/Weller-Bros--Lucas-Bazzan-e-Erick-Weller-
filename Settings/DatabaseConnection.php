ef4c7fcd7e70e64043196e5e38857d94524f0ac4
ShinyKodama <lbazzan.lucas@gmail.com>
2026-09-08 17:56:51 -0300

    a

--- Settings/DatabaseConnection.php
+++ Settings/DatabaseConnection.php
@@ -1,14 +1,15 @@
 <?php 
 $host = "localhost";
 $user_name = "root";
-$user_pass = "root";
+$user_pass = "1234";
 $database = "Filmes";
+$port = 3307;
 
-$con = mysqli_connect($host, $user_name, $user_pass, $database);
+$con = mysqli_connect($host, $user_name, $user_pass, $database, $port);
 
 if (!$con)
     die("Falha na conexão: ". mysqli_connect_error());
 
-mysqli_set_charset($con, "utf8mb4");
+mysqli_set_charset($con, "utf8");
 
 ?>
